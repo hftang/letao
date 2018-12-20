ca190609c27ef113bf5cfa0ea988f69359064614
hftang <617775384@qq.com>
2018-12-20 23:10:05 +0800

    day01 13 首页 和 分类列表的左边完成

--- public/mm/js/index.js
+++ public/mm/js/index.js
@@ -5,4 +5,9 @@ $(function () {
         indicators: false, //是否显示滚动条
     });
 
+    var gallery = mui('.mui-slider');
+    gallery.slider({
+        interval:1000//自动轮播周期，若为0则不自动播放，默认为0；
+    });
+
 })
